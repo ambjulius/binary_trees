7d10d4f5b9cc602c49a2ed71d1132f4750ca6a2d
ambjulius <itaman.julius@gmail.com>
2022-08-27 05:07:35 -0700

    Binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,11 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - checks if a node is a leaf
- * @node: pointer to the node to check
+ * main - Entry point
  *
- * Return: 1 if node is a leaf, and 0 otherwise. If node is NULL, return 0
+ * Return: Always 0 (Success)
  */
+
+int main(void)
+
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node != NULL && node->left == NULL && node->right == NULL)
